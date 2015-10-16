00bc74675bb6ce8a16bbd11703cd91a18d053654
NovaS <nova.saputra@gmail.com>
2015-10-16 11:53:06 +0700

    replace view resolver jsp to thymeleaf template; adjust lib dependency in maven pom file; replace web page index.jsp with index.html; update java configuration for SpringMvcConfig.java;

--- src/main/java/net/novasaputra/springmvc/config/SpringMvcConfig.java
+++ src/main/java/net/novasaputra/springmvc/config/SpringMvcConfig.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 /**
  * @author novas
@@ -20,11 +22,27 @@ public class SpringMvcConfig extends WebMvcConfigurerAdapter {
 		registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 	
-	@Bean
-	public InternalResourceViewResolver jspViewResolver() {
-		InternalResourceViewResolver bean = new InternalResourceViewResolver();
-		bean.setPrefix("/WEB-INF/pages/");
-		bean.setSuffix(".jsp");
-		return bean;
+	@Bean 
+	public ServletContextTemplateResolver templateResolver() {
+		ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+		resolver.setPrefix("/WEB-INF/pages/");
+		resolver.setSuffix(".html");
+		resolver.setTemplateMode("HTML5");
+		resolver.setOrder(1);
+		return resolver;
+	}
+	
+	@Bean 
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setTemplateResolver(templateResolver());
+		return engine;
+	}
+	
+	@Bean 
+	public ThymeleafViewResolver thymeleafViewResolver() {
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine());
+		return resolver;
 	}
 }
